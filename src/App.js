52fd32142189a62490b8ff352fc04b2f5d0f7b56
gstech0322 <gs.tech.0322@gmail.com>
2023-04-05 04:02:58 +0200

    add dropdown

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function App() {
 
   const [betsData, setBetsData] = useState([]);
+
+  const [option, setOption] = useState(0);
 
   useEffect(() => {
     const socket = io();
@@ -40,13 +46,30 @@ function App() {
     new Notification(title, options);
   }
 
-  const getAllData = () => {
+  const getAllData = (optval) => {
     fetch('https://autorefresher.info/api/today_bets')
       .then(response => response.json())
       .then(data => {
         if (Array.isArray(data)) {
-          console.log("Today's bets data:", data);
-          setBetsData(data);
+          console.log("Today's All Bets Data:", data);
+          let resData = data;
+          if (optval === 0) {
+            setBetsData(data);
+          } else if (optval === 1) {
+            resData = resData.filter((item) => 
+              item.sport.includes("NBA") || 
+              item.sport.includes("Basketball")
+            );
+            setBetsData(resData);
+          } else if (optval == 2) {
+            resData = resData.filter((item) => 
+              item.sport.includes("MLB") || 
+              item.sport.includes("Football")
+            );
+            setBetsData(resData);
+          } else {
+            setBetsData(data);
+          }
         } else {
           console.error("Internal Server Error:", data);
         }
@@ -56,8 +79,30 @@ function App() {
       })
   }
 
+  useEffect(() => {
+    if (betsData && betsData.length > 0) {
+      getAllData(option);
+    }
+  }, [option]);
+
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  }
+
   return (
     <TableContainer component={Paper}>
+      <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={option}
+          onChange={handleChange}
+        >
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={1}>NBA</MenuItem>
+          <MenuItem value={2}>MLB</MenuItem>
+        </Select>
+      </FormControl>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -73,46 +118,46 @@ function App() {
         <TableBody>
           {betsData && betsData.length > 0 && betsData.map((row, index) => (
             <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
-                component="th" 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                component="th"
                 scope="row"
               >
-                {index+1}
+                {index + 1}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
-                component="th" 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                component="th"
                 scope="row"
               >
                 {row.date_placed.split(".")[0].replace("T", " ")}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
                 align="right"
               >
                 {row.sport}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }} 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }}
                 align="right"
               >
                 {row.description}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
                 align="right"
               >
                 {row.win_amount}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
                 align="right"
               >
                 {row.website}
               </TableCell>
-              <TableCell 
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }} 
+              <TableCell
+                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
                 align="right"
               >
                 {row.user}
