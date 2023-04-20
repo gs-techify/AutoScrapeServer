6a3fa164821750077cfaf639c36269a1f87128ef
gstech0322 <gs.tech.0322@gmail.com>
2023-04-19 23:58:52 -0700

    add login form ui

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import './App.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  MenuItem,
+  FormControl,
+  Select,
+  Container,
+  TextField,
+  Button
+} from '@mui/material';
 
 function App() {
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState(false);
   const [betsData, setBetsData] = useState([]);
-
   const [option, setOption] = useState(0);
 
   useEffect(() => {
-    const socket = io();
-    socket.on("new bet", body => {
-      console.log("------------new bet-----------------");
-      if (body.isNew) {
-        getAllData();
-        notification(body);
-      }
-    });
-    getAllData();
-  }, []);
+    if (status) {
+      const socket = io();
+      socket.on("new bet", body => {
+        console.log("------------new bet-----------------");
+        if (body.isNew) {
+          getAllData();
+          notification(body);
+        }
+      });
+      getAllData();
+    }
+  }, [status]);
 
   const notification = (new_data) => {
     if (!("Notification" in window)) {
@@ -55,14 +64,14 @@ function App() {
           if (optval === 0) {
             setBetsData(data);
           } else if (optval === 1) {
-            resData = resData.filter((item) => 
-              item.sport.includes("NBA") || 
+            resData = resData.filter((item) =>
+              item.sport.includes("NBA") ||
               item.sport.includes("Basketball")
             );
             setBetsData(resData);
           } else if (optval === 2) {
-            resData = resData.filter((item) => 
-              item.sport.includes("MLB") || 
+            resData = resData.filter((item) =>
+              item.sport.includes("MLB") ||
               item.sport.includes("Football")
             );
             setBetsData(resData);
@@ -88,91 +97,144 @@ function App() {
     setOption(event.target.value);
   }
 
+  const login = () => {
+    setStatus(true);
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
-        <Select
-          labelId="demo-select-small"
-          id="demo-select-small"
-          value={option}
-          onChange={handleChange}
-        >
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={1}>NBA</MenuItem>
-          <MenuItem value={2}>MLB</MenuItem>
-        </Select>
-      </FormControl>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>ID</TableCell>
-            <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DATA PLACED</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>SPORT</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DESCRIPTION</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WIN AMOUNT</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WEBSITE</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>USER</TableCell>
-            <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>GROUP</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {betsData && betsData.length > 0 && betsData.map((row, index) => (
-            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                component="th"
-                scope="row"
+    <>
+      {
+        !status ?
+          <Container
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh'
+            }}
+          >
+            <FormControl>
+              <TextField
+                label='Username'
+                placeholder='Enter username'
+                variant='standard'
+                onChange={(e) => { setUsername(e.target.value) }}
+                fullWidth
+                required
+              />
+              <TextField
+                label='Password'
+                placeholder='Enter password'
+                variant='standard'
+                type='password'
+                onChange={(e) => { setPassword(e.target.value) }}
+                style={{
+                  marginTop: '8px'
+                }}
+                fullWidth
+                required
+              />
+              <Button
+                type='submit'
+                color='primary'
+                variant='outlined'
+                onClick={login}
+                style={{
+                  marginTop: '8px'
+                }}
+                fullWidth
               >
-                {index + 1}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                component="th"
-                scope="row"
+                Sign in
+              </Button>
+            </FormControl>
+          </Container>
+          :
+          <TableContainer component={Paper}>
+            <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+              <Select
+                labelId="demo-select-small"
+                id="demo-select-small"
+                value={option}
+                onChange={handleChange}
               >
-                {row.date_placed.split(".")[0].replace("T", " ")}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                align="right"
-              >
-                {row.sport}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }}
-                align="right"
-              >
-                {row.description}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                align="right"
-              >
-                {row.win_amount}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                align="right"
-              >
-                {row.website}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                align="right"
-              >
-                {row.user}
-              </TableCell>
-              <TableCell
-                style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                align="right"
-              >
-                {row.group}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <MenuItem value={0}>All</MenuItem>
+                <MenuItem value={1}>NBA</MenuItem>
+                <MenuItem value={2}>MLB</MenuItem>
+              </Select>
+            </FormControl>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>ID</TableCell>
+                  <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DATA PLACED</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>SPORT</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DESCRIPTION</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WIN AMOUNT</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WEBSITE</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>USER</TableCell>
+                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>GROUP</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {betsData && betsData.length > 0 && betsData.map((row, index) => (
+                  <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      component="th"
+                      scope="row"
+                    >
+                      {index + 1}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      component="th"
+                      scope="row"
+                    >
+                      {row.date_placed.split(".")[0].replace("T", " ")}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      align="right"
+                    >
+                      {row.sport}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }}
+                      align="right"
+                    >
+                      {row.description}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      align="right"
+                    >
+                      {row.win_amount}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      align="right"
+                    >
+                      {row.website}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      align="right"
+                    >
+                      {row.user}
+                    </TableCell>
+                    <TableCell
+                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                      align="right"
+                    >
+                      {row.group}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+      }
+    </>
   );
 }
 
