b5e755d6f6fb080f6a7dacf1024830edb0e8d0dd
gs-techify <gstechify@outlook.com>
2026-05-18 05:35:59 -0600

    add screenshots

--- src/App.js
+++ src/App.js
@@ -22,22 +22,39 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [status, setStatus] = useState(false);
-  const [betsData, setBetsData] = useState([]);
+  const [betsData, setBetsData] = useState([
+    {
+      date_placed: "05/17/2026 15:08",
+      sport: "horse racing",
+      description: "horse racing",
+      win_amount: 500,
+      website: "1betvegas.com",
+      user: "fl264",
+      group: "N",
+    },
+    {
+      date_placed: "05/17/2026 15:08",
+      sport: "horse racing",
+      description: "horse racing",
+      win_amount: 300,
+      website: "1betvegas.com",
+      user: "fl264",
+      group: "N",
+    }
+  ]);
   const [option, setOption] = useState(0);
 
   useEffect(() => {
-    if (status) {
-      const socket = io();
-      socket.on("new bet", body => {
-        console.log("------------new bet-----------------");
-        if (body.isNew) {
-          getAllData();
-          notification(body);
-        }
-      });
-      getAllData();
-    }
-  }, [status]);
+    const socket = io();
+    socket.on("new bet", body => {
+      console.log("------------new bet-----------------");
+      if (body.isNew) {
+        getAllData();
+        notification(body);
+      }
+    });
+    getAllData();
+  }, []);
 
   const notification = (new_data) => {
     if (!("Notification" in window)) {
@@ -55,6 +72,8 @@ function App() {
   }
 
   const getAllData = (optval) => {
+    console.log("optval+=======", optval);
+
     fetch('https://autorefresher.info/api/today_bets')
       .then(response => response.json())
       .then(data => {
@@ -109,7 +128,7 @@ function App() {
       }),
       redirect: 'follow'
     };
-    
+
     fetch("https://autorefresher.info/api/auth", requestOptions)
       .then(response => response.text())
       .then(result => {
@@ -125,135 +144,135 @@ function App() {
   return (
     <>
       {
-        !status ?
-          <Container
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh'
-            }}
-          >
-            <FormControl>
-              <TextField
-                label='Username'
-                placeholder='Enter username'
-                variant='standard'
-                onChange={(e) => { setUsername(e.target.value) }}
-                fullWidth
-                required
-              />
-              <TextField
-                label='Password'
-                placeholder='Enter password'
-                variant='standard'
-                type='password'
-                onChange={(e) => { setPassword(e.target.value) }}
-                style={{
-                  marginTop: '8px'
-                }}
-                fullWidth
-                required
-              />
-              <Button
-                type='submit'
-                color='primary'
-                variant='outlined'
-                onClick={login}
-                style={{
-                  marginTop: '8px'
-                }}
-                fullWidth
-              >
-                Sign in
-              </Button>
-            </FormControl>
-          </Container>
-          :
-          <TableContainer component={Paper}>
-            <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
-              <Select
-                labelId="demo-select-small"
-                id="demo-select-small"
-                value={option}
-                onChange={handleChange}
-              >
-                <MenuItem value={0}>All</MenuItem>
-                <MenuItem value={1}>NBA</MenuItem>
-                <MenuItem value={2}>MLB</MenuItem>
-              </Select>
-            </FormControl>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>ID</TableCell>
-                  <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DATA PLACED</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>SPORT</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DESCRIPTION</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WIN AMOUNT</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WEBSITE</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>USER</TableCell>
-                  <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>GROUP</TableCell>
+        // !status ?
+        //   <Container
+        //     style={{
+        //       display: 'flex',
+        //       justifyContent: 'center',
+        //       alignItems: 'center',
+        //       height: '100vh'
+        //     }}
+        //   >
+        //     <FormControl>
+        //       <TextField
+        //         label='Username'
+        //         placeholder='Enter username'
+        //         variant='standard'
+        //         onChange={(e) => { setUsername(e.target.value) }}
+        //         fullWidth
+        //         required
+        //       />
+        //       <TextField
+        //         label='Password'
+        //         placeholder='Enter password'
+        //         variant='standard'
+        //         type='password'
+        //         onChange={(e) => { setPassword(e.target.value) }}
+        //         style={{
+        //           marginTop: '8px'
+        //         }}
+        //         fullWidth
+        //         required
+        //       />
+        //       <Button
+        //         type='submit'
+        //         color='primary'
+        //         variant='outlined'
+        //         onClick={login}
+        //         style={{
+        //           marginTop: '8px'
+        //         }}
+        //         fullWidth
+        //       >
+        //         Sign in
+        //       </Button>
+        //     </FormControl>
+        //   </Container>
+        //   :
+        <TableContainer component={Paper}>
+          <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+            <Select
+              labelId="demo-select-small"
+              id="demo-select-small"
+              value={option}
+              onChange={handleChange}
+            >
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={1}>NBA</MenuItem>
+              <MenuItem value={2}>MLB</MenuItem>
+            </Select>
+          </FormControl>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>ID</TableCell>
+                <TableCell style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DATA PLACED</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>SPORT</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>DESCRIPTION</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WIN AMOUNT</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>WEBSITE</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>USER</TableCell>
+                <TableCell align="right" style={{ fontSize: '0.8rem', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px' }}>GROUP</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {betsData && betsData.length > 0 && betsData.map((row, index) => (
+                <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    component="th"
+                    scope="row"
+                  >
+                    {index + 1}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    component="th"
+                    scope="row"
+                  >
+                    {row.date_placed}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    align="right"
+                  >
+                    {row.sport}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }}
+                    align="right"
+                  >
+                    {row.description}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    align="right"
+                  >
+                    {row.win_amount}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    align="right"
+                  >
+                    {row.website}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    align="right"
+                  >
+                    {row.user}
+                  </TableCell>
+                  <TableCell
+                    style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
+                    align="right"
+                  >
+                    {row.group}
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {betsData && betsData.length > 0 && betsData.map((row, index) => (
-                  <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      component="th"
-                      scope="row"
-                    >
-                      {index + 1}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      component="th"
-                      scope="row"
-                    >
-                      {row.date_placed.split(".")[0].replace("T", " ")}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      align="right"
-                    >
-                      {row.sport}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px', textTransform: 'uppercase' }}
-                      align="right"
-                    >
-                      {row.description}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      align="right"
-                    >
-                      {row.win_amount}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      align="right"
-                    >
-                      {row.website}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      align="right"
-                    >
-                      {row.user}
-                    </TableCell>
-                    <TableCell
-                      style={{ fontSize: '0.75rem', paddingTop: '3px', paddingBottom: '3px' }}
-                      align="right"
-                    >
-                      {row.group}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       }
     </>
   );
